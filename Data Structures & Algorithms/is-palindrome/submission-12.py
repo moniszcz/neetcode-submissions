92e4489d061a850422cb16310e1786b323b35062
moniszcz <49846719+moniszcz@users.noreply.github.com>
2026-09-24 10:48:10 +0200

    Add: is-palindrome - submission-12

---
+++ Data Structures & Algorithms/is-palindrome/submission-12.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        def is_alphanum(char: str) -> bool:
+            return (ord('a') <= ord(char) <= ord('z') 
+                    or ord('0') <= ord(char) <= ord('9'))
+        
+        left, right = 0, len(s) - 1
+        s = s.lower()
+
+        while left < right:
+            while left < right and not is_alphanum(s[left]):
+                left += 1
+            
+            while right > left and not is_alphanum(s[right]):
+                right -= 1
+            
+            if s[right] != s[left]:
+                return False
+            else:
+                right -= 1
+                left += 1
+        
+        return True
+
+        
